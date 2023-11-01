104f737a8a936713d3cd915e9fb1210ab22c7407
Tranduclan2@gmail.com <tranduclan2@gmail.com>
2023-11-01 18:21:51 +0700

    Create getAll, getId, Add, Delete , Update comment, Search, update, delete, user

--- src/main/java/com/PolyRepo/PolyRepo/repository/UserRepository.java
+++ src/main/java/com/PolyRepo/PolyRepo/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.PolyRepo.PolyRepo.repository;
 
+import com.PolyRepo.PolyRepo.Entity.CommentEntity;
 import com.PolyRepo.PolyRepo.Entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -7,6 +8,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity,Integer> {
@@ -14,6 +16,7 @@ public interface UserRepository extends JpaRepository<UserEntity,Integer> {
 //    List<UserEntity> getUserByEmail(String email);
 
     UserEntity findByEmail(String email);
+    Optional<UserEntity> findById(Integer id);
     @Query("SELECT u FROM Users u WHERE lower(u.username) LIKE lower(concat('%', :query, '%')) OR lower(u.email) LIKE lower(concat('%', :query, '%'))")
     List<UserEntity> searchUserByNameOrEmail(@Param("query") String query);
 }
--- src/main/java/com/PolyRepo/PolyRepo/service/UserService.java
+++ src/main/java/com/PolyRepo/PolyRepo/service/UserService.java
@@ -1,10 +1,13 @@
 package com.PolyRepo.PolyRepo.service;
 
 
+import com.PolyRepo.PolyRepo.Entity.CommentEntity;
 import com.PolyRepo.PolyRepo.Entity.RoleEntity;
 import com.PolyRepo.PolyRepo.Entity.UserEntity;
 import com.PolyRepo.PolyRepo.exception.CustomException;
 import com.PolyRepo.PolyRepo.payload.request.SignupRequest;
+import com.PolyRepo.PolyRepo.payload.request.UserRequest;
+import com.PolyRepo.PolyRepo.payload.response.CommentResponse;
 import com.PolyRepo.PolyRepo.payload.response.UserResponse;
 import com.PolyRepo.PolyRepo.repository.CommentRepository;
 import com.PolyRepo.PolyRepo.repository.UserRepository;
@@ -12,9 +15,12 @@ import com.PolyRepo.PolyRepo.service.imp.UserServiceImp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService implements UserServiceImp {
@@ -25,7 +31,8 @@ public class UserService implements UserServiceImp {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
-
+    @Autowired
+    private UserRepository roleRepository;
 
     @Override
     public boolean addUser(SignupRequest request) {
@@ -70,6 +77,26 @@ public class UserService implements UserServiceImp {
         }
     }
 
+
+    @Override
+    public UserResponse getUserById(Integer id) {
+        Optional<UserEntity> user = userRepository.findById(id);
+        // Chuyển đổi đối tượng User thành UserResponse để trả về
+        UserResponse userResponse = new UserResponse();
+        userResponse.setId(user.get().getId());
+        userResponse.setEmail(user.get().getEmail());
+        userResponse.setName(user.get().getUsername());
+       userResponse.setRoleId(user.get().getId());
+        return userResponse;
+    }
+
+    @Override
+    public void deleteUserById(Integer id) {
+        UserEntity userEntity = userRepository.findById(id)
+                .orElseThrow(() -> new CustomException("Không tìm thấy comment với ID: " + id));
+        userRepository.delete(userEntity);
+    }
+
     public List<UserResponse> searchUserByNameOrEmail(String query) {
         try {
             List<UserResponse> listUser = new ArrayList<>();
@@ -87,4 +114,30 @@ public class UserService implements UserServiceImp {
         }
     }
 
+
+    @Override
+    public UserResponse updateUser(Integer id, UserRequest userRequest) {
+        UserEntity userEntity = userRepository.findById(id)
+                .orElseThrow(() -> new CustomException("User with ID " + id + " not found"));
+
+        RoleEntity roleEntity = roleRepository.findById(userRequest.getRoleId())
+                .orElseThrow(() -> new CustomException("Role with ID " + userRequest.getRoleId() + " not found")).getRole();
+
+        userEntity.setRole(roleEntity);
+        userEntity.setUsername(userRequest.getName());
+        userEntity.setEmail(userRequest.getEmail());
+//        userEntity.setPasswords(userRequest.getPassword());
+        userEntity.setPasswords(passwordEncoder.encode(userRequest.getPassword()));
+
+        UserEntity updatedUser = userRepository.save(userEntity);
+
+        UserResponse userResponse = new UserResponse();
+        userResponse.setId(updatedUser.getId());
+        userResponse.setName(updatedUser.getUsername());
+
+        userResponse.setRoleId(updatedUser.getRole().getId());
+        userResponse.setEmail(updatedUser.getEmail());
+
+        return userResponse;
+    }
 }
